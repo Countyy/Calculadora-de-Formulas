983caf12524592b75a4cfbd50780873be8c81f31
João Pedro <joaopedr1234567@gmail.com>
2022-06-19 13:14:50 -0300

    commit

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function hipotenusa() {
     const cateto1 = document.getElementById("number1").value;
     const cateto2 = document.getElementById("number2").value;
     const sqr = cateto1 * cateto1 + cateto2 * cateto2
-    const result = Math.sqrt(sqr)
-
+    const result = Math.sqrt(sqr)   
     document.getElementById("output").setAttribute("value", result)
 }
