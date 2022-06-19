b3b3d49c71341dc5b8a70365d1b0f41111e8d3d2
João Pedro <joaopedr1234567@gmail.com>
2022-06-18 22:21:54 -0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("olá")
+function hipotenusa() {
+    const cateto1 = document.getElementById("number1").value;
+    const cateto2 = document.getElementById("number2").value;
+    const sqr = cateto1 * cateto1 + cateto2 * cateto2
+    const result = Math.sqrt(sqr)
+
+    document.getElementById("output").setAttribute("value", result)
+}
