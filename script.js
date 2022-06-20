5391fd36fa07a6189dab1d63c16606588fa4b2eb
João Pedro <joaopedr1234567@gmail.com>
2022-06-20 19:33:38 -0300

    commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function hipotenusa() {
-    const cateto1 = document.getElementById("number1").value;
-    const cateto2 = document.getElementById("number2").value;
-    const sqr = cateto1 * cateto1 + cateto2 * cateto2
-    const result = Math.sqrt(sqr)   
-    document.getElementById("output").setAttribute("value", result)
+    const cateto1 = document.getElementById("number1").value;           // Pega o valor do primeiro cateto
+    const cateto2 = document.getElementById("number2").value;           // Pega o valor do segundo cateto
+    const sqr = cateto1 * cateto1 + cateto2 * cateto2                   // Diz ao programa a fórmula que ele deve utilizar 
+    const result = Math.sqrt(sqr)                                       // Faz a raíz quadrada da fórmula
+    document.getElementById("output").setAttribute("value", result)     // Coloca o resultado no campo de "output" do aplicativo
 }
